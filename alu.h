982e889b9104e311ce1224d1a41f7b449512bf06
Marco <zhangjingxiang25@outlook.com>
2024-10-12 19:10:08 +0800

    ALU_ADD

---
+++ alu.h
@@ -0,0 +1,8 @@
+//
+// Created by zhang on 2024/10/12.
+//
+
+#ifndef ALU_H
+#define ALU_H
+
+#endif //ALU_H
--- constants.h
+++ constants.h
@@ -5,4 +5,16 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+// CPU相关常量
+#define NUM_REGISTERS 32     // 寄存器数量
+#define INSTRUCTION_SIZE 4   // 每条指令占用的字节数
+
+// 缓存相关常量
+#define CACHE_LINE_SIZE 64       // 缓存行大小 64字节
+#define L1_CACHE_LINE_SIZE 16384   // L1缓存大小 16KB
+
+// 虚拟内存相关常量
+#define PAGE_SIZE 4096     // 页面大小 4KB
+#define NUM_PAGES 1024      // 页面数量
+
 #endif //CONSTANTS_H
